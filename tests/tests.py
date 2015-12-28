19992bcc4f9e9bd11d9c2036cf0d5b59f53c7935
realazthat <realazthat@gmail.com>
2015-12-27 21:47:57 -0500

    Trap the logs and extraneous prints from the server code in the unittests, so as not to clutter the unittest output formatting Made new unittest: test_broadcast() Enhanced the test_login() unittest

--- tests/tests.py
+++ tests/tests.py
@@ -29,13 +29,14 @@ class BaseHTTPTestCase(AsyncHTTPTestCase):
 
 
 
-class LoginBroadcastTest(BaseHTTPTestCase):
+class LoginBroadcastTest(BaseHTTPTestCase, LogTrapTestCase):
 
 
     def setUp(self):
 
         #this calls the base class's setUp
         BaseHTTPTestCase.setUp(self)
+        LogTrapTestCase.setUp(self)
         
 
         
@@ -47,20 +48,15 @@ class LoginBroadcastTest(BaseHTTPTestCase):
 
 
 
-    @tornado.testing.gen_test(timeout=60)
-    def test_login_broadcast(self):
+    @tornado.testing.gen_test(timeout=20)
+    def test_login(self):
 
         client = AsyncHTTPClient(self.io_loop)
 
-        #message = {"type": "login", "payload": "django"}
-        #data = {"message": message}
 
         message0 = {"type": "login", "payload": "django"}
-        #data = {"message": json.dumps(message), "other_data_for_the_query_string": "value"}
 
-        #examlpe url: /mr.login?message=<message in JSON>&other_data_for_the_query_string=value
         url = self.get_url('/mr.login?message=' + urllib.parse.quote(json.dumps(message0)))
-        #url = self.get_url('/mr.login?' + urllib.parse.urlencode(data))
 
         response = yield client.fetch(url, method='GET')
 
@@ -80,6 +76,9 @@ class LoginBroadcastTest(BaseHTTPTestCase):
             self.fail("Mr. Login gave an invalid response to a login; it was not valid json; response: %s" % repr(response.body))
 
         
+        self.assertTrue( isinstance(login_response, dict), msg=login_response )
+
+
         self.assertTrue( 'success' in login_response, msg=login_response )
         self.assertTrue( isinstance(login_response['success'], bool), msg=login_response )
         self.assertTrue( login_response['success'], msg=login_response )
@@ -88,6 +87,123 @@ class LoginBroadcastTest(BaseHTTPTestCase):
 
         self.assertTrue( 'msg' in login_response, msg=login_response )
         self.assertTrue( isinstance(login_response['msg'], str), msg=login_response )
+
+
+        cache = self.myapp.global_message_buffer.cache
+        self.assertTrue( len(cache) > 0, msg=cache )
+        self.assertTrue( cache[-1]['type'] == message0['type'] )
+        self.assertTrue( cache[-1]['payload'] == message0['payload'] )
+
+
+
+
+    @tornado.testing.gen_test(timeout=20)
+    def test_broadcast(self):
+
+
+
+        message0 = {"type": "login", "payload": "django"}
+
+        @tornado.gen.coroutine
+        def do_login():
+
+            client = AsyncHTTPClient(self.io_loop)
+            #examlpe url: /mr.login?message=<message in JSON>&other_data_for_the_query_string=value
+            url = self.get_url('/mr.login?message=' + urllib.parse.quote(json.dumps(message0)))
+
+            response = yield client.fetch(url, method='GET')
+
+            self.assertEqual(response.code, 200)
+
+
+        yield do_login()
+
+
+
+        client = AsyncHTTPClient(self.io_loop)
+
+        url = self.get_url('/mr.broadcaster?cursor=100002325435')
+        response = yield client.fetch(url, method='GET')
+        self.assertEqual(response.code, 200)
+
+        bodystr = None
+        try:
+            bodystr = response.body.decode("utf-8")
+        except ValueError:
+            self.fail("Mr. Broadcaster gave an invalid response to a broadcast request; it was not valid utf-8; response: %s" % repr(response.body))
+
+
+        broadcast = None
+        try:
+            broadcast = json.loads(bodystr)
+        except ValueError:
+            self.fail("Mr. Broadcaster gave an invalid response to a broadcast request; it was not valid json; response: %s" % repr(response.body))
+
+
+        self.assertTrue( isinstance(broadcast, dict), msg=broadcast )
+
+        self.assertTrue( 'messages' in broadcast, msg=broadcast )
+        self.assertTrue( isinstance(broadcast['messages'], list), msg=broadcast )
+
+
+        found_login_message = False
+
+        for message in broadcast['messages']:
+
+            self.assertTrue( isinstance(message, dict), msg=message )
+
+
+
+            self.assertTrue( 'id' in message, msg=message )
+            self.assertTrue( isinstance(message['id'], (int,str)), msg=message )
+
+
+
+            self.assertTrue( 'type' in message, msg=message )
+            self.assertTrue( isinstance(message['type'], str), msg=message )
+            self.assertTrue( message['type'] in ['login'], msg=message )
+
+
+
+            self.assertTrue( 'payload' in message, msg=message )
+
+            if (message['type'] == 'login'):
+                self.assertTrue( isinstance(message['payload'], str), msg=message )
+
+
+
+            if message['type'] == 'login' and message['payload'] == 'django':
+                found_login_message = True
+
+
+
+        self.assertTrue(found_login_message
+                            , msg='Did not find the expected login message that we gave Mr. login; message0: %s, broadcast: %s'
+                                     % (message0, broadcast))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
